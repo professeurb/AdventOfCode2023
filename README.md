9dfa08fc183142b191fd8cad63425c9e285484d1
professeurb <olibrunet@free.fr>
2023-12-02 22:18:56 +0100

    Day two, Python and OCaml

--- README.md
+++ README.md
@@ -17,3 +17,15 @@ The first version uses the standard library. We search for string patterns using
 The second version uses the experimental [`brucore`](https://github.com/professeurb/brucore) library which provides some extra functionalities (generators, dynamic arrays, etc.).
 
 Not totally convinced, but it works.
+
+## Day 2
+
+### Python
+
+The use of string splitting w.r.t. another string, together with dictionnaries, leads to a rather straightforward program.
+
+### OCaml
+
+Here, the situation is more complicated, since the splitting function for strings is not as versatile as in Python. This leads to a very complicated and unefficient program.
+
+However, if you use a decent parser library (as I have done with [Angstrom](https://github.com/inhabitedtype/angstrom)), things become much simpler as can be seen in the second version, where the parsing of the input file is no longer the hardest part to write.
---
+++ adc02b.ml
@@ -0,0 +1,76 @@
+(* let file = "test02.txt" *)
+let file = "input02.txt"
+
+let make_bag =
+  List.fold_left
+    (fun (r, g, b) -> function
+      | r', "red" -> (r', g, b)
+      | g', "green" -> (r, g', b)
+      | b', "blue" -> (r, g, b')
+      | _ -> failwith "wrong color")
+    (0, 0, 0)
+
+module Parser = struct
+  open Angstrom
+
+  let many_with_sep p sep =
+    fix (fun m ->
+        List.cons <$> p <*> sep *> m
+        <|> let+ a = p in
+            [ a ])
+
+  let integer =
+    let is_digit = function '0' .. '9' -> true | _ -> false in
+    let+ number = take_while1 is_digit in
+    int_of_string number
+
+  let header = string "Game " *> integer <* string ": "
+
+  let one_color =
+    let+ number = integer <* char ' '
+    and+ color =
+      string "red" <|> string "green" <|> string "blue"
+    in
+    (number, color)
+
+  let bag = many_with_sep one_color (string ", ")
+  let bags = many_with_sep (bag >>| make_bag) (string "; ")
+
+  let line =
+    let+ id = header and+ bags = bags in
+    (id, bags)
+end
+
+let is_possible =
+  List.for_all (fun (r, g, b) -> r <= 12 && g <= 13 && b <= 14)
+
+let eval parser str =
+  match
+    Angstrom.(parse_string ~consume:Consume.All parser str)
+  with
+  | Ok r -> r
+  | Error err -> failwith err
+
+let power bags =
+  let r, g, b =
+    List.fold_left
+      (fun (a, b, c) (a', b', c') ->
+        (max a a', max b b', max c c'))
+      (0, 0, 0) bags
+  in
+  r * g * b
+
+let _ =
+  let data = open_in file in
+  let sum1 = ref 0 and sum2 = ref 0 in
+  try
+    while true do
+      let line = input_line data in
+      let id, bags = eval Parser.line line in
+      if is_possible bags then sum1 := !sum1 + id;
+      sum2 := !sum2 + power bags
+    done
+  with End_of_file ->
+    close_in data;
+    Printf.printf "Part one: %d\n" !sum1;
+    Printf.printf "Part two: %d\n" !sum2
